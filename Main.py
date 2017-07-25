df64fc6337bb203ce3b5008142bff1ed8a661766
Teranet <mbayakelvin@gmail.com>
2017-07-25 11:37:26 +0300

    Changed all scripts to be saved on one directory :ie: /home//crons/

--- Main.py
+++ Main.py
@@ -53,14 +53,15 @@ import subprocess
 import os
 import ConfigParser
 import sys
+
+
+
+
+
+
 """
 Read the Config file and return a config object
 """
-
-
-
-
-
 def configs():
     config = ConfigParser.ConfigParser()
     config.read('config.ini')
@@ -92,26 +93,34 @@ def RunCommands(command):
 
 def addCron(cron,ssh,backupdir,location):
         fileName =location.replace('/','_').strip()
-        bc_sh_content =  "tt=(\`date +%Y-%b-%d-%H-%M-%S\`) ;"+"cd "+ location 
-        +" ;"+"bfile="+backupdir+"/"+fileName
-        +"_\$tt.tar.gz; tar --exclude ./bc.sh  -zcvf \$bfile . ;  scp \$bfile "
-        +MainConfigs['username']+"@"+MainConfigs['backupserver']+":"
-        bcfile = location+"/bc.sh"
+        bc_sh_content =  "tt=(\`date +%Y-%b-%d-%H-%M-%S\`) ;"+"cd "+ location+" ;bfile="+backupdir+"/"+fileName +"_\$tt.tar.gz; tar   -zcvf \$bfile . ;  scp \$bfile "+MainConfigs['username']+"@"+MainConfigs['backupserver']+":"
+        #We keep all the shell scripts on user's home directory. This is to avoid write/execute permmision issues.
+        cronscriptsCommand = ssh + " ' cd ~ ; pwd  '"
+        p = subprocess.Popen(cronscriptsCommand, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+        userHome = p.stdout.read()
+       #Let's create the directory crons if it doesn't exist
+        cronDir = str(userHome).strip()+"/crons/"
+        createDIR = ssh +" 'if  [ ! -d "+cronDir+" ]; then  mkdir -p  "+cronDir +"; else exit 0 ; fi ' "
+        if (RunCommands(createDIR) != 0):
+            print "We are unable to create DIR : "+cronDir+" \n"
+            sys.exit()
+        
+        bcfile = cronDir+fileName+".sh"
         createshCommand = ssh + " 'cd "+location+ "  && echo \""+bc_sh_content+"\" > "+bcfile+"' "
         createsh = RunCommands(createshCommand)
        
        
         if createsh == 0:
-            createCronCm = ssh + " \" crontab -l > tmp ; echo '"
-            +cron+" bash "+location+"/bc.sh' >> tmp ; crontab tmp ; rm -rf tmp \""
+            createCronCm = ssh + " \" crontab -l > tmp ; echo '"+cron+" bash "+bcfile+" ' >> tmp ; crontab tmp ; rm -rf tmp \""
             cronRes = RunCommands(createCronCm)
             if cronRes == 0:
                 print ttime+"| "+bcfile+" ::Cron installed successfully"
             else:
                 print ttime+"|  Unable to install the cron for ::: "+bcfile
+                sys.exit()
         else:
-            print ttime+"| We are unable to generate bc.sh file on the server,"
-            +" check the permissions maybe: Here is the response::: \n"+createsh
+            print ttime+"| We are unable to generate "+bcfile+ " file on the server, check the permissions maybe: Here is the response::: \n"+createsh
+            sys.exit()
 
 
 
